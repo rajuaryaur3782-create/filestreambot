66059963f204f08bf7104b8444a53bea74a31ec9
rajuaryaur3782-create <rajuaryaur3782@gmail.com>
2026-03-12 13:18:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ asyncio.set_event_loop(asyncio.new_event_loop())
 from pyrogram import Client, filters
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 bot = Client(
     "filestreambot",
@@ -18,12 +18,17 @@ bot = Client(
 @bot.on_message(filters.video | filters.document)
 async def get_file(client, message):
 
-    file_id = message.video.file_id if message.video else message.document.file_id
+    if message.video:
+        file_id = message.video.file_id
+    else:
+        file_id = message.document.file_id
 
     link = f"https://filestreambot-skvy.onrender.com/watch/{file_id}"
 
     await message.reply_text(
         f"🎬 Stream Link\n{link}"
     )
+
+print("Bot Started Successfully")
 
 bot.run()
