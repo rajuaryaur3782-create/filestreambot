67683950c1aed2939adfbc1c6840abad6cc5aa55
rajuaryaur3782-create <rajuaryaur3782@gmail.com>
2026-03-12 14:00:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,15 @@ import asyncio
 asyncio.set_event_loop(asyncio.new_event_loop())
 
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import os
 
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+BOT_USERNAME = "filesstreams_bot"   # अपना bot username डालें
+BASE_URL = "https://filestreambot-skvy.onrender.com"
 
 bot = Client(
     "filestreambot",
@@ -15,6 +19,7 @@ bot = Client(
     bot_token=BOT_TOKEN
 )
 
+# file receive
 @bot.on_message(filters.video | filters.document)
 async def get_file(client, message):
 
@@ -23,12 +28,36 @@ async def get_file(client, message):
     else:
         file_id = message.document.file_id
 
-    link = f"https://filestreambot-skvy.onrender.com/watch/{file_id}"
+    unlock_url = f"{BASE_URL}/unlock/{file_id}"
 
-    await message.reply_text(
-        f"🎬 Stream Link\n{link}"
+    keyboard = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("🔓 Watch 2 Ads To Unlock", url=unlock_url)]]
     )
 
-print("Bot Started Successfully")
+    await message.reply_text(
+        "⚠️ Watch 2 ads to unlock video link",
+        reply_markup=keyboard
+    )
+
+# start command (ads देखने के बाद)
+@bot.on_message(filters.command("start"))
+async def start(client, message):
+
+    data = message.text.split(" ")
+
+    if len(data) > 1:
+
+        file_id = data[1]
+
+        link = f"{BASE_URL}/watch/{file_id}"
+
+        await message.reply_text(
+            f"🎬 Stream Link\n{link}"
+        )
+
+    else:
+        await message.reply_text(
+            "Send me a video or file to generate stream link."
+        )
 
 bot.run()
