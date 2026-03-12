b59dd776cf8a02f19f02a52677fbd82bcd4065b3
rajuaryaur3782-create <rajuaryaur3782@gmail.com>
2026-03-12 13:19:03 +0530

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -4,4 +4,11 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"status":"bot running"}
+    return {"status": "bot running"}
+
+@app.get("/watch/{file_id}")
+def watch(file_id: str):
+    return {
+        "stream_file_id": file_id,
+        "message": "Streaming system working"
+    }
